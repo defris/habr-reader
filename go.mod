9b7b8c39ceb9a450e26af1d77ea9117593aac390
defris <defris777@gmail.com>
2026-04-24 14:43:55 +0300

    Added RSS parser

--- go.mod
+++ go.mod
@@ -1,3 +1,23 @@
 module github.com/defris/habr-reader
 
 go 1.26.2
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/gdamore/encoding v1.0.1 // indirect
+	github.com/gdamore/tcell/v2 v2.13.9 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/lucasb-eyer/go-colorful v1.3.0 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/mmcdole/gofeed v1.3.0 // indirect
+	github.com/mmcdole/goxpp v1.1.1-0.20240225020742-a0c311522b23 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/rivo/tview v0.42.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/term v0.37.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
 
-func main() {
-    fmt.Println("Hello, World!")
+	"github.com/mmcdole/gofeed"
+)
+
+// Article представляет структуру статьи
+type Article struct {
+	Title   string
+	Link    string
+	Tags    []string
+	Content string
 }
 
+func fetchRSSFeed(url string) ([]Article, error) {
+	fp := gofeed.NewParser()
+	feed, err := fp.ParseURL(url)
+	if err != nil {
+		return nil, fmt.Errorf("ошибка парсинга RSS: %w", err)
+	}
+
+	var articles []Article
+	for _, item := range feed.Items {
+		// Извлекаем категории/теги из RSS
+		var tags []string
+		for _, category := range item.Categories {
+			tags = append(tags, category)
+		}
+
+		articles = append(articles, Article{
+			Title:   item.Title,
+			Link:    item.Link,
+			Tags:    tags,
+			Content: item.Description,
+		})
+	}
+
+	return articles, nil
+}
+
+func main() {
+	rssURL := "https://habr.com/ru/rss/all/all/"
+
+	articles, err := fetchRSSFeed(rssURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Найдено статей: %d\n", len(articles))
+	for i, article := range articles[:5] { // Покажем первые 5
+		fmt.Printf("%d. %s\n%s\n", i+1, article.Title, article.Tags)
+	}
+}
