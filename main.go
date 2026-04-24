f6cb3c4b38be35da6612964003e58ee3365153b6
defris <defris777@gmail.com>
2026-04-24 17:48:46 +0300

    Added RSS parser by tags

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
+	"time"
 
 	"github.com/mmcdole/gofeed"
 )
 
-// Article представляет структуру статьи
 type Article struct {
 	Title   string
 	Link    string
@@ -15,42 +17,78 @@ type Article struct {
 	Content string
 }
 
-func fetchRSSFeed(url string) ([]Article, error) {
-	fp := gofeed.NewParser()
-	feed, err := fp.ParseURL(url)
+func loadTagsFromFile(filename string) ([]string, error) {
+	data, err := os.ReadFile(filename)
 	if err != nil {
-		return nil, fmt.Errorf("ошибка парсинга RSS: %w", err)
+		return nil, err
 	}
 
-	var articles []Article
-	for _, item := range feed.Items {
-		// Извлекаем категории/теги из RSS
-		var tags []string
-		for _, category := range item.Categories {
-			tags = append(tags, category)
+	var config struct {
+		TargetTags []string `json:"target_tags"`
+	}
+
+	if err := json.Unmarshal(data, &config); err != nil {
+		return nil, err
+	}
+
+	return config.TargetTags, nil
+}
+
+func fetchRSSFeedByTags(targetTags []string) ([]Article, error) {
+	result := []Article{}
+	if len(targetTags) == 0 {
+		return result, fmt.Errorf("Тэги не заполнены")
+	}
+
+	for _, tag := range targetTags {
+		time.Sleep(1 * time.Second)
+		rssURL := "https://habr.com/ru/rss/hub/" + tag + "/articles/all/"
+		log.Printf("Парсинг хаба: %s", tag)
+		fp := gofeed.NewParser()
+		feed, err := fp.ParseURL(rssURL)
+		if err != nil {
+			log.Printf("Ошибка парсинга для тега %s: %v", tag, err)
+			continue
 		}
+		for _, item := range feed.Items {
+			// Извлекаем категории/теги из RSS
+			var tags []string
+			for _, category := range item.Categories {
+				tags = append(tags, category)
+			}
 
-		articles = append(articles, Article{
-			Title:   item.Title,
-			Link:    item.Link,
-			Tags:    tags,
-			Content: item.Description,
-		})
+			result = append(result, Article{
+				Title:   item.Title,
+				Link:    item.Link,
+				Tags:    tags,
+				Content: item.Description,
+			})
+		}
 	}
 
-	return articles, nil
+	return result, nil
 }
 
 func main() {
-	rssURL := "https://habr.com/ru/rss/all/all/"
-
-	articles, err := fetchRSSFeed(rssURL)
+	targetTags, err := loadTagsFromFile("config.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Найдено статей: %d\n", len(articles))
-	for i, article := range articles[:5] { // Покажем первые 5
-		fmt.Printf("%d. %s\n%s\n", i+1, article.Title, article.Tags)
+	articles, err := fetchRSSFeedByTags(targetTags)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Найдено статей: %d\n", len(articles))
+
+	allTags := []string{}
+	for _, article := range articles {
+		allTags = append(allTags, article.Tags...)
+	}
+
+	for i, article := range articles {
+		log.Printf("%d. %s\n%s\n%s\n", i+1, article.Title, article.Link, article.Tags)
+		log.Println("_______________________________________________________________")
 	}
 }
